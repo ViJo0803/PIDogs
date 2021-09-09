f02889ebcfd3ee5c71ebf57f04f96197d864eb6d
SViJo0803 <vijo967@gmail.com>
2021-09-09 15:16:42 -0500

    css

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import style from "./landingPage.module.css";
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className={style.capa}>
       <h1>Bienvenidos a mi Pagina</h1>
       <Link to="/home">
         <button>Ingresar</button>
